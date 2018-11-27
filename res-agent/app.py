6be1d21485edcce87ff520cdccc1b7ad57641c7c
sudhakso <sonu.sudhakaran@hpe.com>
2018-11-28 00:32:05 +0530

    res-agent poc

--- res-agent/app.py
+++ res-agent/app.py
@@ -4,7 +4,7 @@ from os import listdir
 from os.path import isfile, join
 from datetime import datetime
 import subprocess
-
+import requests
 
 import threading
 import json
@@ -17,7 +17,6 @@ from flask import Flask, jsonify, request
 import time
 from watchdog.observers import Observer  
 from watchdog.events import PatternMatchingEventHandler 
-from dirwatcher import Watcher
 
 app = Flask(__name__)
 
@@ -29,13 +28,43 @@ RES_LOG                        = "/var/log/resagent.log"
 # data = {"network": [], "domain": [], "volume": []}
 @app.before_first_request
 def activate_watcher():
-    w = Watcher(app.logger)
+    
     def run_watcher():
-        while True:
-            app.logger.info("Discovering new / updated resources in your environment ...")
-            w.run()
+        try:
+            while True:
+                app.logger.info("Discovering new / updated resources in your environment ...")
+                if os.listdir(RES_AGENT_RESOUIRCES_DIR):
+                    try:
+                        # export the file
+                        inventory = [f for f in listdir(RES_AGENT_RESOUIRCES_DIR) if isfile(RES_AGENT_RESOUIRCES_DIR + f)]
+                        for afile in inventory:
+                            with open(RES_AGENT_RESOUIRCES_DIR + afile) as json_file:
+                                json_data = json.load(json_file)
+                                payload = {afile : json_data}
+                                # Send HTTP request
+                                if 'DISCO_SERVER' in os.environ:
+                                    base_url= os.enviro["DISCO_SERVER"]
+                                    final_url="/{0}/node/upload/{1}".format(base_url,os.environ['HOSTNAME'])
+                                    response = requests.post(final_url, data=payload)
+                                    app.logger.info("Response %s :", response.text) #TEXT/HTML
+                                    app.logger.info("Response Code = %d, Reason = %s", response.status_code, response.reason) #HTTP
+                                else:
+                                    app.logger.error("ERROR: Set the environment variable DISCO_SERVER")
+                                    break
+                    except Exception as e:
+                        app.logger.error("Format Exception : %s", str(e))
+                else:
+                    app.logger.info("Nothing new to upload...")
+                # graceful sleep
+                time.sleep(10)
+        except KeyboardInterrupt:
+            sys.exit(0)
+            
     thread = threading.Thread(target=run_watcher)
-    thread.start()
+    try:
+        thread.start()
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 @app.route('/')
 def hello():
--- res-agent/dirwatcher.py
+++ res-agent/dirwatcher.py
@@ -1,7 +1,10 @@
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from multiprocessing import Lock
 
+
+isChanged = False
 
 class Watcher:
 
@@ -9,6 +12,7 @@ class Watcher:
 
     def __init__(self, logger):
         self.observer = Observer()
+        self.logger = logger
 
     def run(self):
         event_handler = Handler()
@@ -17,9 +21,16 @@ class Watcher:
         try:
             while True:
                 time.sleep(10)
+#                 with Lock:
+#                     if isChanged:
+#                         # Raise a toast to update the content
+#                         self.logger.info("Detected a change in configuration ....")
+#                         isChanged = False
+                self.logger.info("Watching changes %s ....", isChanged)
+                isChanged = False
         except:
             self.observer.stop()
-            logger.info("Stopping watcher")
+            self.logger.info("Stopping watcher")
 
         self.observer.join()
 
@@ -30,11 +41,7 @@ class Handler(FileSystemEventHandler):
     def on_any_event(event):
         if event.is_directory:
             return None
-
-        elif event.event_type == 'created':
-            # Take any action here when a file is first created.
-            logger.info("Received created event - %s." % event.src_path)
-
-        elif event.event_type == 'modified':
+        elif event.event_type == 'created' or event.event_type == 'modified':
             # Taken any action here when a file is modified.
-            logger.info("Received modified event - %s." % event.src_path)
+            print "Received created / modified event - %s." % event.src_path
+            isChanged = True
